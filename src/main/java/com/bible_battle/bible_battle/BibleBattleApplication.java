a823ef8ea98f5699a80ae171073c92dcdb6ca5e0
Dot <Ndodangcani@outlook.com>
2026-07-15 01:45:43 +0200

    Iniial commit #2

---
+++ src/main/java/com/bible_battle/bible_battle/BibleBattleApplication.java
@@ -0,0 +1,13 @@
+package com.bible_battle.bible_battle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleBattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bible_battle/bible_battle/BibleBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bible_battle.bible_battle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
